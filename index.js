d98f11a28fb4eca2d7e758e9ca200403e9ea8b38
ganeshtambekar <ganesh.tambekar124@gmail.com>
2024-09-29 15:19:08 +0530

    env file added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
+const env=require('dotenv').config()
 const app = express()
-const port = 3000
+// const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -9,6 +10,6 @@ app.get('/flipkart',(req,res)=>{
     res.send("<h1>this is the flipkart page</h1>")
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(process.env.PORT||3000, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`)
 })
